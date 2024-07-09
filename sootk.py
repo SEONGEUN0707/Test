deacf0c62f9048960cd68431a009767044fcb7b1
SeongeunPark <seongeunpark0716@gmail.com>
2024-07-09 20:54:32 +0900

    Select and Shoot

--- sootk.py
+++ sootk.py
@@ -11,21 +11,20 @@ pygame.init()
 WIDTH, HEIGHT = 1500, 750
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
-#마우스가 그리는 선 길이
+# 마우스가 그리는 선 길이
 def calculate_distance(p1,p2):
     return math.sqrt((p2[1] - p1[1])**2 + (p2[0] - p1[0])**2)
 
-#그 길이 앵글
+# 그 길이 앵글
 def calculate_angle(p1,p2):
-    return math.atan2(p2[1] - p1[1] , p2[0] - p1[0])
+    return math.atan2(p2[1] - p1[1], p2[0] - p1[0])
 
 def visualize_velocity(window, balls, font, scale_factor: float, is_pause) -> None:
     for ball in balls:
-        # Get velocity and position of each ball
+
         vel_x, vel_y = ball[0].body.velocity
         pos_x, pos_y = ball[0].body.position
 
-        # Write mass and velocity
         mass_info = font.render(f"mass: {ball[0].mass}", True, (0, 0, 0))
         if is_pause:
             vel_info = font.render(f"vel: {round(math.sqrt(math.pow(vel_x, 2) + math.pow(vel_y, 2)), 2)}", True, (0, 0, 0))
@@ -37,12 +36,9 @@ def visualize_velocity(window, balls, font, scale_factor: float, is_pause) -> No
                 window.blit(vel_info, (pos_x - 10, pos_y - 50))
         window.blit(mass_info, (pos_x - 10, pos_y - 70))
         
-
-        # Make guideline pointing ball's moving direction
         pygame.draw.line(window, (225, 0, 0), (pos_x, pos_y), (pos_x + vel_x * scale_factor, pos_y + vel_y * scale_factor))
 
-
-#배경 그리기
+# 배경 그리기
 def draw(space, window, draw_options):
     window.fill("white")
     space.debug_draw(draw_options)
@@ -50,16 +46,14 @@ def draw(space, window, draw_options):
 def create_segment(space, first_pos, line):
     if line:
         body = pymunk.Body(body_type=pymunk.Body.STATIC)
-        #body.position = pos
-        shape = pymunk.Segment(body, line[0], line[1], 3)#3은 두께
+        shape = pymunk.Segment(body, line[0], line[1], 3)  # 3은 두께
         shape.elasticity = 0
         shape.friction = 0
         space.add(body, shape)
     if first_pos:
-        pygame.draw.line(window, "black", first_pos, pygame.mouse.get_pos(), 3)#3은 두께
-        
+        pygame.draw.line(window, "black", first_pos, pygame.mouse.get_pos(), 3)  # 3은 두께
 
-#배경 벽
+# 배경 벽
 def create_boundaries(space, width, height):
     rects = [
         [(width / 2, height - 2.5), (width, 5)],
@@ -72,26 +66,29 @@ def create_boundaries(space, width, height):
         body = pymunk.Body(body_type=pymunk.Body.STATIC)
         body.position = pos
         shape = pymunk.Poly.create_box(body, size)
-        shape.elasticity = 0
+        shape.elasticity = 0.1
         shape.friction = 0
         space.add(body, shape)
 
-#볼1 만들기
+# 볼1 만들기
 def create_Ball(space, radius, mass, pos, balls, is_pause):
     if is_pause:
-        body = pymunk.Body(body_type=pymunk.Body.DYNAMIC) #is_pause가 True이면 DYNAMIC
+        body = pymunk.Body(body_type=pymunk.Body.DYNAMIC)  # is_pause가 True이면 DYNAMIC
     else:
         body = pymunk.Body(body_type=pymunk.Body.STATIC)
     body.position = pos
     shape = pymunk.Circle(body, radius)
     shape.mass = mass
-    shape.elasticity = 0.9
-    shape.color = (255, 0 ,0, 100)
+    shape.elasticity = 0
+    shape.color = (199, 199, 199, 100)
     space.add(body, shape)
     balls.append([shape, None])
     return shape, balls
 
-#main
+def draw_line(window, first_pos):
+    pygame.draw.line(window, "black", first_pos, pygame.mouse.get_pos(), 3)
+
+# main
 def run(window, width, height):
     run = True
     clock = pygame.time.Clock()
@@ -101,35 +98,22 @@ def run(window, width, height):
     is_pause = False
     mode = 1
 
+    selected_shape = None
+
     balls = []
 
     pygame.display.set_caption("Sootk")
     Font = pygame.font.SysFont("Consolas", 20, True, False)
 
     space = pymunk.Space()
-    space.gravity = (0,2000)
+    space.gravity = (0, 2000)
 
     create_boundaries(space, width, height)
 
     draw_options = pymunk.pygame_util.DrawOptions(window)
     
     while run:
-
-        if mode % 2 == 1:
-            Text = Font.render("Mode : Create_Ball", True, (34,139,34))
-        elif mode % 2 == 0:
-            Text = Font.render("Mode : Draw_Lines", True, (34,139,34))
-        
-        if is_pause:
-            Text_1 = Font.render("STATUS : MOVE", True, (34,139,34))
-        else:
-            Text_1 = Font.render("STATUS : STOP", True, (34,139,34))
-        
-        window.blit(Text, (10,10))
-        window.blit(Text_1, (10,30))
-        visualize_velocity(window, balls, Font, 0.1, is_pause)
-        
-        pygame.display.update()
+        draw(space, window, draw_options)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -137,14 +121,28 @@ def run(window, width, height):
                 break
 
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                if mode % 2 == 0:
+                if mode % 3 == 1:
+                    selected_shape = None
+                    create_Ball(space, 30, 10, pygame.mouse.get_pos(), balls, is_pause)
+
+                elif mode % 3 == 2:
+                    selected_shape = None
                     if not first_pos:
                         first_pos = pygame.mouse.get_pos()
                     else:
                         line = [first_pos, pygame.mouse.get_pos()]
                         first_pos = None
-                else:
-                    create_Ball(space,30,10,pygame.mouse.get_pos(),balls, is_pause)
+                
+                elif mode % 3 == 0:
+                    if near_shape and selected_shape is None and isinstance(near_shape, pymunk.Circle):
+                        selected_shape = near_shape
+                    elif selected_shape:
+                        angle = calculate_angle(selected_shape.body.position, pygame.mouse.get_pos())
+                        force = calculate_distance(selected_shape.body.position, pygame.mouse.get_pos()) * 50
+                        fx = math.cos(angle) * force
+                        fy = math.sin(angle) * force
+                        selected_shape.body.apply_impulse_at_local_point((fx, fy), (0, 0))
+                        selected_shape = None
 
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_p:
@@ -165,13 +163,44 @@ def run(window, width, height):
                 line = None
                 first_pos = None
 
+        if mode % 3 == 1:
+            Text = Font.render("Mode : Create_Ball", True, (34, 139, 34))
+        elif mode % 3 == 2:
+            Text = Font.render("Mode : Draw_Lines", True, (34, 139, 34))
+        elif mode % 3 == 0:
+            Text = Font.render("Mode : Shoot_Ball", True, (34, 139, 34))
 
-        draw(space, window, draw_options)
+        if is_pause:
+            Text_1 = Font.render("STATUS : MOVE", True, (34, 139, 34))
+        else:
+            Text_1 = Font.render("STATUS : STOP", True, (34, 139, 34))
+
+        window.blit(Text, (10, 10))
+        window.blit(Text_1, (10, 30))
+        visualize_velocity(window, balls, Font, 0.1, is_pause)
+
+        if mode % 3 == 0 and selected_shape is None:
+            # Ball 감지
+            mouse_pos = pymunk.pygame_util.get_mouse_pos(window)
+            near_shape = space.point_query_nearest(
+                mouse_pos, float("inf"), pymunk.ShapeFilter()
+            ).shape
+            if near_shape is not None and isinstance(near_shape, pymunk.Circle):
+                r = near_shape.radius + 4
+                pygame.draw.circle(window, pygame.Color("blue"), near_shape.body.position, int(r), 2)
+
+        elif mode % 3 == 0 and selected_shape:
+            r = selected_shape.radius + 4
+            pygame.draw.circle(window, pygame.Color("red"), selected_shape.body.position, int(r), 2)
+
+        if selected_shape:
+            draw_line(window, selected_shape.body.position)
+
         create_segment(space, first_pos, line)
-
         space.step(dt)
         clock.tick(fps)
+        pygame.display.update()
 
-#인터프리터에서 직접 실행시 실행
+# 인터프리터에서 직접 실행시 실행
 if __name__ == "__main__":
     run(window, WIDTH, HEIGHT)
